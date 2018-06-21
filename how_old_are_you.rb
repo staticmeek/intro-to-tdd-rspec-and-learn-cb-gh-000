179bddf64b31615e50b447f38ecaaa2d92d0f8b5
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-21 05:33:59 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,10 +1,7 @@
-def current_age_for_birth_year(birth_year)
-  2003 - birth_year
-end
-describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
+require_relative './current_age_for_birth_year.rb'
+puts "What year were you born?"
+birth_year = gets.to_i
 
-    expect(age_of_person).to eq(19)
-  end
-end
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
